848481f2f08ce69c47041fddf20a9bd2dcf23138
Lucho2708 <luis.silva.2708@gmail.com>
2021-04-13 14:34:34 -0500

    2 - Instalación de LIVEWIRE y Bootstrap con NPM | Rimorsoft Online

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="{{asset('css/app.css')}}" rel="stylesheet">
+
+        <!-- Styles -->
+        @livewireStyles
+    </head>
+    <body>
+        @yield('content')
+
+        @livewireScripts
+    </body>
+</html>
