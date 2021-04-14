0436da90d0c05a221b53f34c7e6609e404a51967
Lucho2708 <luis.silva.2708@gmail.com>
2021-04-13 20:21:30 -0500

    4 - Consulta y PAGINACIÓN usando LARAVEL LIVEWIRE | Rimorsoft Online

--- app/Http/Livewire/PostComponent.php
+++ app/Http/Livewire/PostComponent.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Http\Concerns\InteractsWithInput;
 use Livewire\Component;
+use Livewire\WithPagination;
+use App\Models\Post;
 
 class PostComponent extends Component
 {
+    use WithPagination;
+
+    protected $paginationTheme = 'bootstrap';
+
     public function render()
     {
-        return view('livewire.post-component');
+        return view('livewire.post-component', [
+            'posts'=>Post::orderBy('id', 'desc')->paginate(8)
+        ]);
     }
 }
---
+++ resources/views/livewire/table.blade.php
@@ -0,0 +1,28 @@
+<h1>Listado de Posts</h1>
+<table class="table table-striped">
+    <thead>
+    <tr>
+        <th>#</th>
+        <th>Titulo</th>
+        <th>Contenido</th>
+        <th colspan="2">&nbsp;</th>
+    </tr>
+    </thead>
+    <tbody>
+        @foreach($posts as $post)
+            <tr>
+                <td>{{$post->id}}</td>
+                <td>{{$post->title}}</td>
+                <td>{{$post->body}}</td>
+                <td>
+                    <button class="btn btn-primary">Editar</button>
+                </td>
+                <td>
+                    <button class="btn btn-danger">Eliminar</button>
+                </td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
+
+{{$posts->links()}}
